fe850e22b02d275fead8f4abdb4c6bf644c0c10e
worknarouchon <worknarouchon@gmail.com>
2020-04-04 23:36:56 +0700

    Edit num 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,26 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { NweactivityPage } from '../pages/nweactivity/nweactivity';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { IonicStorageModule } from '@ionic/storage'
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NweactivityPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NweactivityPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { NweactivityPage } from '../../pages/nweactivity/nweactivity';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+todolist:any[]=[];
+temp:any={title:"",description:"",price:"",note:""};
 
-  constructor(public navCtrl: NavController) {
-
+constructor(public navCtrl: NavController, public storage:Storage) {
+    this.storage.get('todoDetails').then((val)=>{
+      this.todolist=val;
+    });
   }
+    
+ngOnInit() {
+  this.storage.get('todoDetails').then((val)=>{
+    this.todolist=val;
+});
+}
+ionVieWillEnter(){
+  this.storage.get('todoDetails').then((val)=>{
+    this.todolist=val;
+  });
+}
 
+ionViewDidEnter(){
+  this.storage.get('todoDetails').then((val)=>{
+    this.todolist=val;
+  });
+}
+
+gotoNweactivity(){
+  this.navCtrl.push(NweactivityPage);
+}
+removeItem(todo){
+  let index = this.todolist.indexOf(todo);
+  if(index>-1){
+    this.todolist.splice(index,1);
+    this.storage.set('todoDetails',this.todolist);
+  }
+}
 }
